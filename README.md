3737764d23aafb84e4cfdccf320c1d95acb21961
Alex Harvey <alexharv074@gmail.com>
2019-05-22 23:20:44 +1000

    Add Readme
    https://stackoverflow.com/a/56255240/3787051

---
+++ README.md
@@ -0,0 +1,17 @@
+# Serverspec-test
+
+A repo where I test Serverspec code.
+
+## Usage
+
+Install deps:
+
+```text
+▶ bundle install
+```
+
+Run Serverspec:
+
+```text
+▶ bundle exec rspec spec/localhost/sample_spec.rb
+```
--- spec/localhost/sample_spec.rb
+++ spec/localhost/sample_spec.rb
@@ -1,5 +1,16 @@
-require 'spec_helper'
+require "spec_helper"
 
-describe command('curl -s -k -I http://alexharvey3118.s3-website-ap-southeast-2.amazonaws.com/chats.html') do
-  its(:stdout) { should match /200/ }
+describe file("/etc/passwd") do
+  it { is_expected.to be_file }
+  its(:content) { is_expected.to match /root/ }
+end
+
+describe "passwd file" do
+  it { expect(file("/etc/passwd")).to be_file }
+  it { expect(file("/etc/passwd").content).to match /root/ }
+end
+
+describe file("/etc/passwd") do
+  it { expect(subject).to be_file }
+  it { expect(subject.content).to match /root/ }
 end
